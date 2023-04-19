789218435831a50142d6d2fa6f25c88c08c8c829
Jose Arancibia suazo <mjarancibiasuazo@gmail.com>
2023-04-18 23:44:38 -0400

    segundo commit 18-04-23

--- src/components/todoApp.js
+++ src/components/todoApp.js
@@ -1,25 +1,58 @@
 import { useState } from "react";
+import Todo from "./todo";
 
 export default function TodoApp() {
-//title estado que queremos obtener y setTitle estado que queremos cambiar
+    //title estado que queremos obtener y setTitle estado que queremos cambiar
     const [title, setTitle] = useState('hola');
 
-    function handleClick(e){
-        e.preventDefault();
-        setTitle("Jose")
+    //Estado de lista de tareas
+    const [todos, setTodos] = useState([]);
+
+    function handleChange(e) {
+        const value = e.target.value;
+
+        setTitle(value);
     }
 
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        const newTodo = {
+            id: crypto.randomUUID(),
+            title: title,
+            completed: false
+        };
+
+        //Hacemos una copia de todos
+        const temp = [...todos];
+        //Agregar en una nueva lista los todos
+        temp.unshift(newTodo);
+        setTodos(temp);
+    }
+
+
     return (<div className="todoContainer">
-        <form className="todoCreateForm">
-            <input className="todoInput" value={title}/>
-            <input onClick={handleClick} 
-            type="submit" 
-            alue="Create todo" 
-            className="buttonCreate" 
+        <form className="todoCreateForm" onSubmit={handleSubmit}>
+
+            <input onChange={handleChange} className="todoInput" value={title} />
+            <input
+                onClick={handleSubmit}
+                type="submit"
+                alue="Create todo"
+                className="buttonCreate"
             />
-        
-            {title}
+
         </form>
+
+        <div className="todosContainer">
+            {
+                todos.map(item => (
+                    <Todo key={ item.id } item = {item} />
+                ))
+            }
+
+        </div>
+
 
     </div>
     );
